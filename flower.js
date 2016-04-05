009d0edcd29e36db080538443bc92da93866234c
Torey Hickman <torey@toreyhickman.com>
2016-04-05 13:58:48 -0500

    Add flower constructor and test skeleton

---
+++ flower.js
@@ -0,0 +1,1 @@
+var Flower = function() {}
---
+++ spec/javascripts/flower_spec.js
@@ -0,0 +1,10 @@
+describe("a flower", function() {
+  var flower
+
+  beforeEach(function() {
+    flower = new Flower("daffodil", "yellow");
+  });
+
+  xit("has a name", function() {});
+  xit("has a color", function() {});
+});
