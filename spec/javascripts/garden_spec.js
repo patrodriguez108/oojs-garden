0a1a938148a65f80bd79b821be8e46beca88d83c
Torey Hickman <torey@toreyhickman.com>
2016-04-05 13:47:39 -0500

    Convert garden tests to Jasmine

---
+++ spec/javascripts/garden_spec.js
@@ -0,0 +1,67 @@
+describe("the garden object", function() {
+  it("is named Kula Botanical Garden", function() {
+    expect(garden.name).toEqual("Kula Botanical Garden");
+  });
+
+  it("is located in Makawao", function() {
+    expect(garden.location).toEqual("Makawao");
+  });
+
+  describe("has a collection of flowers", function() {
+    it("has an empty collection of flowers", function() {
+      expect(garden.flowers).toEqual([]);
+    });
+
+    describe("adding and removing flowers", function() {
+      afterEach(function() {
+        garden.flowers = [];
+      });
+
+      var daffodil = { name: "daffodil", color: "yellow" };
+      var iris     = { name: "iris", color: "purple" }
+      var aster    = { name: "aster", color: "red" }
+
+      it("can add a flower to its flowers", function() {
+        garden.flowers = [];
+
+        garden.addFlower(daffodil);
+        expect(garden.flowers).toEqual([daffodil]);
+      });
+
+      it("can plant multiple flowers", function() {
+        garden.flowers = [aster];
+        flowersToPlant = [daffodil, iris]
+
+        garden.plant(flowersToPlant);
+        expect(garden.flowers).toHaveSameElementsAs([aster, daffodil, iris])
+      });
+
+      it("can remove a flower", function() {
+        garden.flowers = [daffodil];
+
+        garden.remove(daffodil);
+        expect(garden.flowers).toEqual([]);
+      });
+    });
+
+    describe("filtering its flowers", function() {
+      it("filters flowers by color", function() {
+        var yellowFlower1 = { color: "yellow" };
+        var yellowFlower2 = { color: "yellow" };
+        var redFlower1    = { color: "red" };
+
+        garden.flowers = [yellowFlower1, redFlower1, yellowFlower2];
+        expect(garden.flowersByColor("yellow")).toHaveSameElementsAs([yellowFlower1, yellowFlower2]);
+      });
+
+      it("filters flowers by name", function() {
+        var iris1  = { name: "iris" };
+        var iris2  = { name: "iris" };
+        var aster1 = { name: "aster" };
+
+        garden.flowers = [aster1, iris1, iris2];
+        expect(garden.flowersByName("iris")).toHaveSameElementsAs([iris1, iris2]);
+      });
+    });
+  });
+});
