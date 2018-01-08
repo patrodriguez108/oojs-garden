f9290dd3703affdf538fc2a180695d494ebfb679
Patrick <pat.rodriguez108@gmail.com>
2018-01-08 15:05:09 -0600

    Remove psuedocode

--- src/garden.js
+++ src/garden.js
@@ -1,15 +1,14 @@
-/*
-
-Use object literal syntax to create a garden object with
-the attributes and behaviors described in spec/garden_spec.js.
-
-*/
-
 var garden = {
 	name: "Kula Botanical Garden",
 	location: "Makawao",
 	flowers: [],
 	addFlower: function(flower) {
 		this.flowers.push(flower);
+	},
+	plant: function(bouquet) {
+		console.log(this.flowers);
+		console.log(bouquet);
+		var plants = this.flowers.concat(bouquet);
+		console.log(plants)
 	}
 };
