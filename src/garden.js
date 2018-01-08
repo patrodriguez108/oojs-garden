1ad4a3079555854f428e30732c47018496d8ce91
Patrick <pat.rodriguez108@gmail.com>
2018-01-08 14:57:08 -0600

    Add function to add flower to garden

--- src/garden.js
+++ src/garden.js
@@ -6,5 +6,10 @@ the attributes and behaviors described in spec/garden_spec.js.
 */
 
 var garden = {
-	name: "Kula Botanical Garden"
+	name: "Kula Botanical Garden",
+	location: "Makawao",
+	flowers: [],
+	addFlower: function(flower) {
+		this.flowers.push(flower);
+	}
 };
