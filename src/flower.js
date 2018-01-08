06570cea758a9190d8254befd166da3a87dc9e73
Patrick <pat.rodriguez108@gmail.com>
2018-01-08 14:51:08 -0600

    Remove pseudo

--- src/flower.js
+++ src/flower.js
@@ -1,10 +1,3 @@
-/*
-
-Write a constructor function that produces objects
-matching the description in spec/flower_spec.js.
-
-*/
-
 var Flower = function(name, color) {
 	this.name = name;
 	this.color = color
